82082cda15caedcddf879c4fe6249d0b9ae743cf
fwtan <fuwen.tan@gmail.com>
2018-05-14 17:41:09 -0400

    test load

--- captioning_server_gpu.lua
+++ captioning_server_gpu.lua
@@ -58,100 +58,100 @@ protos.cnn:evaluate()
 protos.lm:evaluate()
 protos.layout_encoder:evaluate()
 
-async.http.listen('http://0.0.0.0:' .. opt.port, function(req,res)
-   print(req.body)
-   local status, annotations = pcall(cjson.decode, req.body)
-   if not status then return end
-   -- print(#annotations)
-   -- batch size is 1
-   -- local bbox_coords = torch.FloatTensor(1,#annotations,4):fill(0)
-   local bbox_coords = torch.FloatTensor(1,96,4):fill(0)
-   local full_category = torch.LongTensor(1,96):fill(0)
-   for k, v in pairs(annotations) do
-      bbox_coords[{1,k,1}] = v['left']
-      bbox_coords[{1,k,2}] = v['top']
-      bbox_coords[{1,k,3}] = v['width']
-      bbox_coords[{1,k,4}] = v['height']
-      full_category[{1,k}] = COCO_label_dict[v['label']]
-   end
-   bbox_coords:div(604.0)
+-- async.http.listen('http://0.0.0.0:' .. opt.port, function(req,res)
+--    print(req.body)
+--    local status, annotations = pcall(cjson.decode, req.body)
+--    if not status then return end
+--    -- print(#annotations)
+--    -- batch size is 1
+--    -- local bbox_coords = torch.FloatTensor(1,#annotations,4):fill(0)
+--    local bbox_coords = torch.FloatTensor(1,96,4):fill(0)
+--    local full_category = torch.LongTensor(1,96):fill(0)
+--    for k, v in pairs(annotations) do
+--       bbox_coords[{1,k,1}] = v['left']
+--       bbox_coords[{1,k,2}] = v['top']
+--       bbox_coords[{1,k,3}] = v['width']
+--       bbox_coords[{1,k,4}] = v['height']
+--       full_category[{1,k}] = COCO_label_dict[v['label']]
+--    end
+--    bbox_coords:div(604.0)
 
-   local bbox_coords_T = bbox_coords:transpose(1,2):cuda()
-   local data_full_category_T = full_category:transpose(1,2):cuda()
-   local layout_feats = protos.layout_encoder:forward({ data_full_category_T, bbox_coords_T })
-   local sample_opts = { sample_max = 1, beam_size = 2, temperature = 1.0 }
-   local seq = protos.lm:sample(layout_feats, sample_opts)
-   local sents = net_utils.decode_sequence(vocab, seq)
-   print(sents[1])
-   res(sents[1], {['Content-Type']='text/html', ['Access-Control-Allow-Origin']='*'})
-   return
+--    local bbox_coords_T = bbox_coords:transpose(1,2):cuda()
+--    local data_full_category_T = full_category:transpose(1,2):cuda()
+--    local layout_feats = protos.layout_encoder:forward({ data_full_category_T, bbox_coords_T })
+--    local sample_opts = { sample_max = 1, beam_size = 2, temperature = 1.0 }
+--    local seq = protos.lm:sample(layout_feats, sample_opts)
+--    local sents = net_utils.decode_sequence(vocab, seq)
+--    print(sents[1])
+--    res(sents[1], {['Content-Type']='text/html', ['Access-Control-Allow-Origin']='*'})
+--    return
       
 
-   -- if (req.body.path == nil and req.body.source == nil) then 
-   -- 	res("LoL", {['Content-Type']='text/html', ['Access-Control-Allow-Origin']='*'})
-   --      return
-   -- end
+--    -- if (req.body.path == nil and req.body.source == nil) then 
+--    -- 	res("LoL", {['Content-Type']='text/html', ['Access-Control-Allow-Origin']='*'})
+--    --      return
+--    -- end
 
-   -- local input = nil
-   -- if (req.body.path) then
-   --    local filename = string.match(req.body.path.data, "[a-zA-Z0-9]+.[a-zA-Z]+$")
-   --    input = torch.Tensor(1, 1, 32, 128)
-   --    status, code = pcall(image.load,filename, 1, nil)
-   --    if not status then
-   -- 	res("LoL", {['Content-Type']='text/html', ['Access-Control-Allow-Origin']='*'})
-   --      return
-   --    end
-   --     input[1] = code
-   -- else
-   --    input = gm.Image():fromString(req.body['source'].data):toTensor('float','I','DHW')
-   --    input = input:float()
-   --  -- im = torch.FloatTensor(48, math.floor(scale*img:size(2)))
-   --  -- image.scale(im, img)
-   --    if input:size(2) ~= 48 then
-   --      new_width = 48 * input:size(3) / input:size(2)
-   --    	input = image.scale(input, new_width, 48)
-   --    end
-   --    --image.save('input.jpg', input)
-   --    local result = predict_seq(input[1])
-   --    --result = '{"boxes":[{"x":1,"y":1,"width":32, "height":32}]}'
-   --    print(result)
-   --    res(result, {['Content-Type']='text/html', ['Access-Control-Allow-Origin']='*'})
-   --    --image.save('kkk.png', input)
-   -- end
+--    -- local input = nil
+--    -- if (req.body.path) then
+--    --    local filename = string.match(req.body.path.data, "[a-zA-Z0-9]+.[a-zA-Z]+$")
+--    --    input = torch.Tensor(1, 1, 32, 128)
+--    --    status, code = pcall(image.load,filename, 1, nil)
+--    --    if not status then
+--    -- 	res("LoL", {['Content-Type']='text/html', ['Access-Control-Allow-Origin']='*'})
+--    --      return
+--    --    end
+--    --     input[1] = code
+--    -- else
+--    --    input = gm.Image():fromString(req.body['source'].data):toTensor('float','I','DHW')
+--    --    input = input:float()
+--    --  -- im = torch.FloatTensor(48, math.floor(scale*img:size(2)))
+--    --  -- image.scale(im, img)
+--    --    if input:size(2) ~= 48 then
+--    --      new_width = 48 * input:size(3) / input:size(2)
+--    --    	input = image.scale(input, new_width, 48)
+--    --    end
+--    --    --image.save('input.jpg', input)
+--    --    local result = predict_seq(input[1])
+--    --    --result = '{"boxes":[{"x":1,"y":1,"width":32, "height":32}]}'
+--    --    print(result)
+--    --    res(result, {['Content-Type']='text/html', ['Access-Control-Allow-Origin']='*'})
+--    --    --image.save('kkk.png', input)
+--    -- end
 
-   -- -- reference
-   -- -- https://github.com/benglard/waffle/blob/master/waffle/request.lua#L34
-   -- -- https://github.com/clementfarabet/graphicsmagick on image:load(tensor,colorSpace,dimensions)
-   -- local prediction = predict(input)
-   -- print(prediction)
-   -- res(prediction, {['Content-Type']='text/html', ['Access-Control-Allow-Origin']='*'})
+--    -- -- reference
+--    -- -- https://github.com/benglard/waffle/blob/master/waffle/request.lua#L34
+--    -- -- https://github.com/clementfarabet/graphicsmagick on image:load(tensor,colorSpace,dimensions)
+--    -- local prediction = predict(input)
+--    -- print(prediction)
+--    -- res(prediction, {['Content-Type']='text/html', ['Access-Control-Allow-Origin']='*'})
 
-   -- async.fs.writeFile(req.body['source'].filename, req.body['source'].data, function()
-   --                       async.process.spawn('open', {req.body['source'].filename}, function()
-   --                                                               end))
+--    -- async.fs.writeFile(req.body['source'].filename, req.body['source'].data, function()
+--    --                       async.process.spawn('open', {req.body['source'].filename}, function()
+--    --                                                               end))
 
-   -- local resp
-   -- if req.url.path == '/test' then
-   --    resp  = [[
-   --    <p>You requested route /test</p>
-   --    ]]
-   -- else
-   --    -- Produce a random story:
-   --    resp = [[
-   --    <h1>From my server</h1>
-   --    <p>It's working!<p>
-   --    <p>Randomly generated number: ${number}</p>
-   --    <p>A variable in the global scope: ${ret}</p>
-   --    ]] % {
-   --       number = math.random(),
-   --       ret = ret
-   --    }
-   -- end
+--    -- local resp
+--    -- if req.url.path == '/test' then
+--    --    resp  = [[
+--    --    <p>You requested route /test</p>
+--    --    ]]
+--    -- else
+--    --    -- Produce a random story:
+--    --    resp = [[
+--    --    <h1>From my server</h1>
+--    --    <p>It's working!<p>
+--    --    <p>Randomly generated number: ${number}</p>
+--    --    <p>A variable in the global scope: ${ret}</p>
+--    --    ]] % {
+--    --       number = math.random(),
+--    --       ret = ret
+--    --    }
+--    -- end
 
-   -- -- res(resp, {['Content-Type']='text/html'})
+--    -- -- res(resp, {['Content-Type']='text/html'})
    
-end)
+-- end)
 
-print('server listening to port ' .. opt.port)
+-- print('server listening to port ' .. opt.port)
 
-async.go()
+-- async.go()
